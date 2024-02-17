7e7ec133d855301962f6461eb33c4b475687ddb1
Logan Mondal Bhamidipaty <76822456+FlyingWorkshop@users.noreply.github.com>
2024-02-17 11:57:53 -0800

    skeleton transition

--- CompressedBeliefPOMDPs/src/solver.jl
+++ CompressedBeliefPOMDPs/src/solver.jl
@@ -3,9 +3,11 @@ using QuickPOMDPs
 using POMDPTools: Deterministic
 
 
-struct CompressedSolver <: Solver
+struct CompressedSolver <: POMDPs.Solver
+    # TODO: probably need a base solver that takes care of the MDP
     compressor::Compressor
     approximator::Approximator
+    updater::POMDPs.Updater
 end
 
 
@@ -17,54 +19,68 @@ end
 #     end
 # end
 
-function solve(solver::CompressedSolver, pomdp::POMDP)
+
+function POMDPs.solve(solver::CompressedSolver, pomdp::POMDP)
     # collect sample beliefs (TODO)
     n_samples, n_states = (10, 5)
-    beliefs = rand(n_samples, n_states)
+    B = rand(n_samples, n_states)
 
     # compress belief space
-    fit!(solver.compressor, beliefs)
-    compressed = compress(solver.compressor, beliefs)
+    fit!(solver.compressor, B)
+    B̃ = compress(solver.compressor, B)
 
-    # convert low-dimensional space into discrete state space
+    # compute state space for belief MDP
     res = 5
     axis = LinRange(0, 1, res)
     grid = reinterpret(reshape, Float64, [(i, j) for i=axis for j=axis])  # TODO: use GridInterpolations.jl
-    approximated = approximate(solver.approximator, grid', compressed)
-    reconstructed = decompress(solver.compressor, approximated)
+    B̃s = approximate(solver.approximator, grid', B̃)
+    # TODO: can I move all of the decompression outside of R and T?
+
+    # compute rewards for belief MDP
+    function R(b̃s, a)
+        r = 0.0
+        b = decompress(solver.compressor, b̃s)
+        for (i, s) in enumerate(states(pomdp))
+            r += reward(pomdp, s, a) * b[i]
+        end
+        return r
+    end
+
+    # compute transitions for belief MDP
+    T̃s = Dict()
+    for b̃s in B̃s, a in actions(pomdp)
+        for o in observations(pomdp)   # TODO: use observations or observation (singular); space or distribution; how to handle both?
+            b = decompress(solver.compressor, b̃s)
+            bp = POMDPs.update(solver.updater, b, a, o)
+            b̃p = compress(solver.compressor, bp)
+            b̃sp = approximate(solver.approximator, b̃p)
+
+            prob = 0.0
+            for sp in states(pomdp)
+                O = observation(a, sp)
+                prob += pdf(O, o)
+                multiplier = 0.0
+                for (i, s) in enumerate(states(pomdp))
+                    T = transition(pomdp, s, a)
+                    multiplier += pdf(T, sp) * b[i]
+                end
+                prob *= multiplier
+            end
+            T̃s[b̃s, a, b̃sp] = prob 
+        end
+    end
+
+    function T(s, a, s')
+        return T̃s[s, a, s']
+    end
 
     # create a low-dimensional belief MDP approximation of the POMDP
-    α = 1  # TODO: put elsewhere
     mdp = QuickMDP(
-        initialstate = initialstate(pomdp),
-        states = states(pomdp),
+        initialstate = Uniform(B̃s),  # TODO: maybe change
+        states = B̃s,
         discount = discount(pomdp),
-        transition = function(s, a)
-
-
-            # NOTE: doesn't work when base (PO)MDP uses an implicit distribution
-            ImplicitDistribution(s, a) do s, a, rng
-                # TODO: get Mykel to double check this
-                T = transition(pomdp, s, a)
-                sp = rand(rng, T)
-                O = observation(pomdp, s, a, sp)  # TODO: ask Mykel if this is the right distribution
-                z = rand(rng, O)
-
-                pdf(d, z)
-
-
-                return s + a
-            end
-        end,
-        reward = function(b̃, a)
-            r = 0.0
-            b = decompress(solver.compressor, b̃)
-            for (i, s) in enumerate(states(pomdp))
-                r += reward(pomdp, s, a) * b[i]
-            end
-            return r
-        end,
-        discount = discount(pomdp),
+        transition = T,
+        reward = R,
     )
 
     return "Hello World!"
